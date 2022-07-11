4dca00194d4944b1e3f91124d314b9b97cdad692
Diego Canez <canezdiego@gmail.com>
2022-07-10 23:57:53 -0500

    Adds mlflow experimentation

--- overfit/models/primingnet.py
+++ overfit/models/primingnet.py
@@ -1,7 +1,9 @@
 import math
+from typing import Optional
 
 import torch
 import torch.nn.functional as F
+from mlflow import log_metric
 from torchvision.models import ResNet152_Weights, resnet152
 
 
@@ -33,7 +35,9 @@ class PrimingNet(torch.nn.Module):
         k = math.e + self.confidence
         return math.log(k) * y_hat
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, y: Optional[int] = None, step: Optional[int] = None
+    ) -> torch.Tensor:
         self.optimizer.zero_grad()
         y_src = self.pretrained_classifier(x)
         y_tgt = y_src + self.prime
@@ -41,11 +45,47 @@ class PrimingNet(torch.nn.Module):
         H_src = torch.distributions.Categorical(logits=y_src).entropy()[0] / math.log2(
             self.num_classes
         )
-        new_lr = self.lr_scale * H_src
-        print(f"Entropy: {H_src}")
-        print(f"Learning Rate: {new_lr}")
-        print(f"Norm: {torch.norm(self.prime)}")
-        print(f"Max: {torch.max(self.prime)}")
+        new_lr = self.lr_scale * (1 - H_src)
+
+        # START logging
+
+        prob = F.softmax(y_tgt, dim=1)
+
+        # top5_prob, top5_catid = torch.topk(probabilities, 5)
+        if step is not None:
+            if y is not None:
+                log_metric("Correct probability", prob.detach().numpy()[0][y], step)
+                log_metric("Correct prime", self.prime.detach().numpy()[y], step)
+            log_metric("Target Prediction Norm", torch.norm(prob), step)
+            log_metric("Target Prediction Max", torch.max(prob).detach().numpy(), step)
+            log_metric("Target Unnormalized Prediction Norm", torch.norm(y_tgt), step)
+            log_metric(
+                "Target Unnormalized Prediction Max",
+                torch.max(y_tgt).detach().numpy(),
+                step,
+            )
+            log_metric(
+                "Source Unnormalized Prediction Max",
+                torch.max(y_src).detach().numpy(),
+                step,
+            )
+            log_metric(
+                "Source Correct Prediction",
+                F.softmax(y_src, dim=1).detach().numpy()[0][y],
+                step,
+            )
+
+            log_metric(
+                "Target Correct Prediction",
+                F.softmax(y_tgt, dim=1).detach().numpy()[0][y],
+                step,
+            )
+            log_metric("Source Prediction Entropy", H_src, step)
+            log_metric("Learning Rate", new_lr, step)
+            log_metric("Prime Norm", torch.norm(self.prime), step)
+            log_metric("Prime Max", torch.max(self.prime).detach().numpy(), step)
+
+        # END logging
         for ix, _ in enumerate(self.optimizer.param_groups):
             self.optimizer.param_groups[ix]["lr"] = new_lr
         pseudo_loss = F.cross_entropy(y_tgt, y_pseudo)

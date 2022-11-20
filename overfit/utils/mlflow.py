90c27da091b9f8501b58871fff2eee19d961d5d8
Diego Canez <canezdiego@gmail.com>
2022-11-19 19:50:32 -0500

    cpu

--- overfit/utils/mlflow.py
+++ overfit/utils/mlflow.py
@@ -5,31 +5,36 @@ from mlflow.entities import Metric
 
 
 def log_max(name: str, x: torch.Tensor, step: int):
-    log_metric(name, torch.max(x).detach().numpy(), step)
+    log_metric(name, torch.max(x).cpu().detach().numpy(), step)
 
 
 def log_norm(name: str, x: torch.Tensor, step: int):
-    log_metric(name, torch.norm(x), step)
+    log_metric(name, torch.norm(x).cpu(), step)
 
 
 def log_idx(name: str, x: torch.Tensor, idx: int, step: int, timestamp):
-    log_metric(name, x.detach().numpy()[idx], step)
+    log_metric(name, x.cpu().detach().numpy()[idx], step)
 
 
 def get_log_max(name: str, x: torch.Tensor, step: int, timestamp: int):
     return Metric(
         key=name,
-        value=float(torch.max(x).detach().numpy()),
+        value=float(torch.max(x).cpu().detach().numpy()),
         timestamp=timestamp,
         step=step,
     )
 
 
 def get_log_norm(name: str, x: torch.Tensor, step: int, timestamp: int):
-    return Metric(key=name, value=float(torch.norm(x)), timestamp=timestamp, step=step)
+    return Metric(
+        key=name, value=float(torch.norm(x).cpu()), timestamp=timestamp, step=step
+    )
 
 
 def get_log_idx(name: str, x: torch.Tensor, idx: int, step: int, timestamp: int):
     return Metric(
-        key=name, value=float(x.detach().numpy()[idx]), timestamp=timestamp, step=step
+        key=name,
+        value=float(x.cpu().detach().numpy()[idx]),
+        timestamp=timestamp,
+        step=step,
     )
